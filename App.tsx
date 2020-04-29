02689f28a51a12c13246c530cc14eb901f79d063
David Govea <govea.d@gmail.com>
2020-04-29 13:17:34 -0700

    wip

--- App.tsx
+++ App.tsx
@@ -1,21 +1,26 @@
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-import React, { useState, useEffect } from 'react';
+import React, {
+  ComponentProps,
+  FunctionComponent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import {
-  Alert,
   Animated,
   Easing,
   LayoutChangeEvent,
+  StatusBar,
   StyleSheet,
   Text,
   View,
-  LayoutRectangle,
 } from 'react-native';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import { t, color } from 'react-native-tailwindcss';
-import { human, iOSUIKit, iOSColors } from 'react-native-typography';
-import Toast from 'react-native-root-toast';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import SafeAreaView from 'react-native-safe-area-view';
+import { color, t } from 'react-native-tailwindcss';
+import { iOSUIKit } from 'react-native-typography';
+
 const DONE_GREEN = '#45c352';
 const GRAY = '#595959';
 const DARK_GRAY = '#202020';
@@ -26,20 +31,22 @@ const GRAY_TEXT = { color: GRAY };
 const BUTTON_HEIGHT = 65;
 const CHECK_HEIGHT = 40;
 const TODO_RADIUS = 25;
+const BORDER_WIDTH = 2;
+const LOADING_BOUNCE_HEIGHT = Math.floor(TODO_RADIUS / 2);
 
 interface Todo {
   name: string;
   date: string;
 }
 const STATIC_DATA: { [k: string]: Todo[] } = {
-  personal: [
+  Personal: [
     { name: 'Caroobi Font Styleguide', date: 'Today' },
     { name: 'Workshop Aquisition Elements', date: 'Tomorrow' },
     { name: 'Icon Design', date: '14 Feb' },
     { name: 'Unify Header Visuals', date: '16 Feb' },
     { name: 'MLH Messaging', date: '20 Feb' },
   ],
-  team: [
+  Team: [
     { name: 'Meeting with David', date: 'Tomorrow' },
     { name: 'New UI Presentation', date: 'Tomorrow' },
     { name: 'Product Planning', date: '16 Feb' },
@@ -48,106 +55,226 @@ const STATIC_DATA: { [k: string]: Todo[] } = {
   ],
 };
 
-export default function App() {
-  const [activeList, setList] = useState<'personal' | 'team'>('personal');
-  const [data, setData] = useState<Array<Todo & { background: string }>>(
-    STATIC_DATA[activeList].map((t, i) => ({
-      ...t,
-      background: GREEN_PALETTE[i],
-    }))
-  );
+const ToggleSwitch: FunctionComponent<{
+  options: [string, string];
+  active: string;
+  onChange: (value: string) => void;
+}> = (props) => {
+  const [firstOpt, secondOpt] = props.options;
+  const firstOptionSelected = props.active === firstOpt;
+  const [animation] = useState(new Animated.Value(firstOptionSelected ? 0 : 1));
+
+  const [width, setWidth] = useState<number>(0);
+  const captureWidth = (layoutEvent: LayoutChangeEvent) => {
+    setWidth(layoutEvent.nativeEvent.layout.width);
+  };
+
+  const createHandler = (opt: string) => {
+    return () => {
+      props.onChange(opt);
+      Animated.timing(animation, {
+        toValue: opt === firstOpt ? 0 : 1,
+        easing: Easing.inOut(Easing.cubic),
+      }).start();
+    };
+  };
+
+  const animatedBackgroundStyle = {
+    transform: [
+      {
+        translateX: animation.interpolate({
+          inputRange: [0, 1],
+          outputRange: [0, width / 2],
+        }),
+      },
+    ],
+  };
+
+  const animatedFirstTextColor = {
+    color: animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: ['#fff', GRAY],
+    }),
+  };
+
+  const animatedSecondTextColor = {
+    color: animation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [GRAY, '#fff'],
+    }),
+  };
 
   return (
-    <SafeAreaProvider>
-      <View style={[t.bgBlack, t.flexGrow]}>
-        <SafeAreaView style={[t.flexRow, t.itemsCenter, t.p8]}>
-          <View style={[t.flexGrow]}>
-            <Text style={[iOSUIKit.subheadEmphasized, t.mB3, GRAY_TEXT]}>
-              Welcome Back
-            </Text>
+    <View style={[styles.listTabContainer]} onLayout={captureWidth}>
+      <Animated.View
+        style={[styles.listTabBackground, animatedBackgroundStyle]}
+      />
+      <View onTouchStart={createHandler(firstOpt)} style={[styles.listTab]}>
+        <Animated.Text style={[styles.listTabText, animatedFirstTextColor]}>
+          {firstOpt}
+        </Animated.Text>
+      </View>
+      <View onTouchStart={createHandler(secondOpt)} style={[styles.listTab]}>
+        <Animated.Text style={[styles.listTabText, animatedSecondTextColor]}>
+          {secondOpt}
+        </Animated.Text>
+      </View>
+    </View>
+  );
+};
 
-            <Text style={[iOSUIKit.largeTitleEmphasizedWhite]}>Karlo</Text>
-          </View>
-          <View style={[styles.circleButton]}>
-            <MaterialCommunityIcons
-              name="arrow-right"
-              style={[t.text3xl, GRAY_TEXT]}
+function prepareData<T>(data: T[], palette: string[]) {
+  return data.map((t, i) => ({
+    ...t,
+    background: palette[i],
+  }));
+}
+
+export default function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [activeList, setList] = useState('Personal');
+  const [data, setData] = useState(
+    prepareData(STATIC_DATA[activeList], GREEN_PALETTE)
+  );
+
+  const loadTimer = useRef<number | null>(null);
+  const changeList = (list: string) => {
+    setList(list);
+    setIsLoading(true);
+    clearTimeout(loadTimer.current);
+    loadTimer.current = setTimeout(() => {
+      const palette = list === 'Personal' ? GREEN_PALETTE : PURPLE_PALETTE;
+      setData(prepareData(STATIC_DATA[list], palette));
+      setIsLoading(false);
+    }, 800);
+  };
+
+  const topCard = data[data.length - 1];
+
+  const [loadingAnimation] = useState(new Animated.Value(isLoading ? 0 : 1));
+  useEffect(() => {
+    Animated.timing(loadingAnimation, {
+      toValue: isLoading ? 1 : 0,
+      duration: 300,
+      easing: Easing.out(Easing.ease),
+    }).start();
+  }, [isLoading]);
+
+  const todoLoadingStyles = {
+    paddingTop: loadingAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [16, 16 + LOADING_BOUNCE_HEIGHT / 2],
+    }),
+    marginBottom: loadingAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [-TODO_RADIUS - LOADING_BOUNCE_HEIGHT, -TODO_RADIUS],
+    }),
+  };
+  const textLoadingStyles = {
+    opacity: loadingAnimation.interpolate({
+      inputRange: [0, 1],
+      outputRange: [1, 0],
+    }),
+  };
+  return (
+    <>
+      <StatusBar translucent={false} barStyle="light-content" />
+      <SafeAreaProvider>
+        <View style={[t.bgBlack, t.flexGrow]}>
+          <SafeAreaView style={[t.flexRow, t.itemsCenter, t.pX8]}>
+            <View style={[t.flexGrow, t.mT3]}>
+              <Text style={[iOSUIKit.subheadEmphasized, t.mB3, GRAY_TEXT]}>
+                Welcome Back
+              </Text>
+
+              <Text style={[iOSUIKit.largeTitleEmphasizedWhite]}>Karlo</Text>
+            </View>
+            <View style={[styles.circleButton]}>
+              <MaterialCommunityIcons
+                name="arrow-right"
+                style={[t.text3xl, GRAY_TEXT]}
+              />
+            </View>
+          </SafeAreaView>
+
+          <ToggleSwitch
+            options={['Personal', 'Team']}
+            active={activeList}
+            onChange={changeList}
+          />
+
+          <View style={[t.flexGrow, t.mX3, t.mT8]}>
+            {data.map((todo) => (
+              <Animated.View
+                style={[
+                  { backgroundColor: todo.background },
+                  styles.todoContainer,
+                  todoLoadingStyles,
+                ]}
+              >
+                <View style={[t.flexRow]}>
+                  <View style={[t.flexGrow]}>
+                    <Animated.Text
+                      style={[
+                        iOSUIKit.subheadEmphasizedWhite,
+                        textLoadingStyles,
+                      ]}
+                    >
+                      {todo.name}
+                    </Animated.Text>
+                    <Animated.Text
+                      style={[
+                        iOSUIKit.footnoteEmphasized,
+                        textLoadingStyles,
+                        { color: 'rgba(255, 255, 255, 0.6)' },
+                      ]}
+                    >
+                      {todo.date}
+                    </Animated.Text>
+                  </View>
+                  <Animated.View style={[styles.todoButton, textLoadingStyles]}>
+                    <MaterialCommunityIcons
+                      name="check"
+                      style={[t.textXl, t.textWhite]}
+                    />
+                  </Animated.View>
+                </View>
+              </Animated.View>
+            ))}
+            <View
+              style={[t.flexGrow, { backgroundColor: topCard.background }]}
             />
           </View>
-        </SafeAreaView>
-        <View style={[styles.listTabContainer]}>
-          <View style={[styles.listTabBackground]} />
-          <View style={[styles.listTab]}>
-            <Text style={[styles.listTabText]}>Personal</Text>
-          </View>
-          <View style={[styles.listTab]}>
-            <Text style={[styles.listTabText]}>Team</Text>
-          </View>
         </View>
-
-        <View style={[t.mX3, t.mT8]}>
-          {data.map((todo) => (
-            <View
-              style={[
-                t.pT4,
-                t.pX5,
-                {
-                  height: BUTTON_HEIGHT + TODO_RADIUS,
-                  borderTopLeftRadius: TODO_RADIUS,
-                  borderTopRightRadius: TODO_RADIUS,
-                  backgroundColor: todo.background,
-                  marginBottom: -TODO_RADIUS,
-                },
-              ]}
-            >
-              <View style={[t.flexRow]}>
-                <View style={[t.flexGrow]}>
-                  <Text style={[iOSUIKit.subheadEmphasizedWhite]}>
-                    {todo.name}
-                  </Text>
-                  <Text
-                    style={[
-                      iOSUIKit.footnoteEmphasized,
-                      { color: 'rgba(255, 255, 255, 0.6)' },
-                    ]}
-                  >
-                    {todo.date}
-                  </Text>
-                </View>
-                <View
-                  style={[
-                    t.justifyCenter,
-                    t.itemsCenter,
-                    {
-                      width: CHECK_HEIGHT,
-                      height: CHECK_HEIGHT,
-                      borderRadius: CHECK_HEIGHT / 2,
-                      backgroundColor: 'rgba(0, 0, 0, 0.1)',
-                    },
-                  ]}
-                >
-                  <MaterialCommunityIcons
-                    name="check"
-                    style={[t.textXl, t.textWhite]}
-                  />
-                </View>
-              </View>
-            </View>
-          ))}
-        </View>
-      </View>
-    </SafeAreaProvider>
+      </SafeAreaProvider>
+    </>
   );
 }
 
 const roundedButtonStyle = {
   height: BUTTON_HEIGHT,
   borderRadius: BUTTON_HEIGHT / 2,
-  borderWidth: 2,
+  borderWidth: BORDER_WIDTH,
   borderColor: DARK_GRAY,
 };
 
 const styles = StyleSheet.create({
+  todoContainer: {
+    ...t.pT4,
+    ...t.pX5,
+
+    height: BUTTON_HEIGHT + TODO_RADIUS + LOADING_BOUNCE_HEIGHT,
+    borderTopLeftRadius: TODO_RADIUS,
+    borderTopRightRadius: TODO_RADIUS,
+  },
+  todoButton: {
+    ...t.justifyCenter,
+    ...t.itemsCenter,
+    width: CHECK_HEIGHT,
+    height: CHECK_HEIGHT,
+    borderRadius: CHECK_HEIGHT / 2,
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+  },
   circleButton: {
     ...t.itemsCenter,
     ...t.justifyCenter,
@@ -157,8 +284,9 @@ const styles = StyleSheet.create({
   listTabContainer: {
     ...t.flexRow,
     ...t.relative,
-    ...t.itemsCenter,
+    ...t.itemsStretch,
     ...t.mX3,
+    ...t.mT8,
     ...roundedButtonStyle,
   },
   listTab: {
@@ -166,16 +294,17 @@ const styles = StyleSheet.create({
     // ...t.flexGrow,
     // ...t.flexShrink0,
     ...t.itemsCenter,
+    ...t.justifyCenter,
   },
   listTabText: {
     ...iOSUIKit.title3EmphasizedObject,
-    color: GRAY,
   },
   listTabBackground: {
     ...t.absolute,
     ...roundedButtonStyle,
+    borderWidth: 0,
     backgroundColor: DARK_GRAY,
     width: '50%',
-    left: 0,
+    top: -BORDER_WIDTH,
   },
 });
